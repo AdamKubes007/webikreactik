aa6a82087748485f7b1e3d998728bc6548372565
AdamKubes007 <adam.kubes.2002@gmail.com>
2023-03-10 17:40:48 +0100

    Zjednoduseni App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './components/Footer/Footer';
+import Main from './components/Main/Main';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <>    
+    <Header/>
+    <Main/>  
+    <Footer/>
+    </>
+
+  );
+}
+
+export default App;
