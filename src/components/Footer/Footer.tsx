04c520f995bd95bac098fea7cc19cef991e02763
AdamKubes007 <adam.kubes.2002@gmail.com>
2023-03-10 15:12:42 +0100

    upravy slozek

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,32 @@
+import './App.css';
+import ikonaFB from "../../assets/images/logos/ikona-fb.jpg";
+import ikonaIG from "../../assets/images/logos/ikona-ig.jpg";
+import { useState } from 'react';
+
+function Footer(){
+    return(
+    <footer>
+        <p>©STUDENTSKÁ UNIE UTB, 2022</p>
+        <div className="contact-links">
+          <a className="mail-link" href="mailto:su@utb.cz">su@utb.cz</a>
+          <div className="socials">
+            <a
+              href="https://www.facebook.com/studentskaunieutb/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={ikonaFB} alt="fb ikona" />
+            </a>
+            <a
+              href="https://www.instagram.com/studentskaunieutb/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={ikonaIG} alt="ig ikona" />
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+}
+export default Footer;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import Ract from "react";
+ 
+function Header(){
+  return (
+    <div className="App">
+      <header>
+        <div className="logo">Čajka Kajka</div>
+        <nav>
+          <ul className="menu-links">
+            <li><a href="Moje projekty">Moje projekty</a></li>
+            <li><a href="Kontakty">Kontakt</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+export default Header;
